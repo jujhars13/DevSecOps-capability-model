43ecc3c19922ef48823b50d85d46e5cd9f653bb8
Jujhar Singh <jujhar@jujhar.com>
2020-07-08 15:00:26 +0100

    moved over nicely to jekyll

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
 # DevSecOps-capability-model
 A simplified capability model for those on DevSecOps transformation journey.
 
+Deployed using github-pages to [https://devsecops.jujhar.com]()
+
+
 ## Thanks
 
 Kudos to the original [Timo Pagel](https://github.com/wurstbrot) for the initial [project](https://github.com/wurstbrot/DevSecOps-MaturityModel).
 
-Deployed using github-pages to [https://devsecops.jujhar.com]()
+Influenced and inspired by
+- Unicorn Project
+- Accelerate
 
+## Deploying
 
-## Building and running
+This site is a [Jekyll static site](https://jekyllrb.com/) and will be auto deployed via Github pages to [https://devsecops.jujhar.com]() upon commit to master
+
+## Local Development
 
 ```bash
 # to server on localhost:4000 and continuously compile pages
-docker run -it --rm -v ${PWD}:/srv/jekyll -p 127.0.0.1:4000:4000 jekyll/jekyll jekyll serve
+docker run -it --rm -v ${PWD}:/srv/jekyll -p 4000:4000 jekyll/jekyll jekyll serve --watch
+
+# OPTIONAL use the awesome `reload` which auto-refreshes your browser on change using websockets
+# `npm install -g reload`
+(cd _site && reload -e "html|js|css|json")
 
 # one time build assets into _site
 docker run --rm -v ${PWD}:/srv/jekyll jekyll/jekyll jekyll build
--- js/table.js
+++ js/table.js
@@ -1,37 +1,53 @@
+/**
+ * Render an HTML table onto the page using D3.js
+ * Could have rendered by hand but d3 give us some nice
+ * abstractions
+ *
+ * @param {string} id document id you want to inject table into
+ * @param {obj} headings array of headings
+ * @param {obj} data the data you wish to render
+ * @param {obj} options options
+ */
 function generateTable(id, headings, data, options) {
-  const cfg = {};
+  const cfg = { sortAscending: true };
 
-  const sortAscending = true;
-  const table = d3.select(id).append("table");
-  const titles = d3.values(headings);
+  const table = d3
+    .select(id)
+    .append("table")
+    .attr("class", "table table-striped");
+
+  // table headers
+  const titles = Object.values(headings);
   const headers = table
     .append("thead")
+    .attr("class", "thead-dark")
     .append("tr")
     .selectAll("th")
     .data(titles)
     .enter()
     .append("th")
-    .text((d)=> d)
+    .text((d) => d)
     .on("click", function (d) {
       headers.attr("class", "header");
 
-      if (sortAscending) {
+      if (cfg.sortAscending) {
         rows.sort(function (a, b) {
           return b[d] < a[d];
         });
-        sortAscending = false;
+        cfg.sortAscending = false;
         this.className = "aes";
       } else {
         rows.sort(function (a, b) {
           return b[d] > a[d];
         });
-        sortAscending = true;
+        cfg.sortAscending = true;
         this.className = "des";
       }
     });
 
   const rows = table
     .append("tbody")
+    .attr("class", "")
     .selectAll("tr")
     .data(data)
     .enter()
